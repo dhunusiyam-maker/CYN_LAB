33d0e5b43dee139c5b21fa1384cbbafed6e1459d
dhunusiyam-maker <dhunusiya.m@gmail.com>
2026-07-10 14:39:36 +0530

    Create casercipher.java

---
+++ casercipher.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ceaserCipher{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter plaintext:")
+    String plainText=sc.nextLine();
+    System.out.print("Enter key:");
+    String key=sc.nextLine();
+    
+  }
+}
+
